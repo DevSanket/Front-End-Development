69022c90307f1c8ab3fa3e79a6e0d8cdbc82e8bb
sanket <programmersanket@gmail.com>
2022-01-26 12:08:35 +0530

    Navigationn menu done with responsive in Blog page project

---
+++ CSS Projects/10 Blog Page/main.js
@@ -0,0 +1,8 @@
+const toggle = document.querySelector('#header .toggle-button');
+
+let collapse = document.querySelectorAll('#header .collapse');
+
+
+toggle.addEventListener('click', function(){
+    collapse.forEach(col => col.classList.toggle("collapse-toggle"))
+})
